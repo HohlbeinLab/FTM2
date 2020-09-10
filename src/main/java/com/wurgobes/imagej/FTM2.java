6900e45585458d80a89b1662b653ae1e33a183f9
dr_njitram <martijn.gobes@gmail.com>
2020-09-10 14:57:35 +0200

    Cleaned up the setup and added some extra info acquisition there. Also replaced the loading of a single frame with a faster method (~20% faster)

--- src/main/java/com/wurgobes/imagej/FTM2.java
+++ src/main/java/com/wurgobes/imagej/FTM2.java
@@ -14,32 +14,22 @@ import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
 import net.haesleinhuepf.clij2.CLIJ2;
 
 import java.awt.image.ColorModel;
-import net.imagej.ops.Op;
+
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
-import io.scif.services.DatasetIOService;
-import org.scijava.ui.UIService;
 import java.io.File;
-import java.io.IOException;
-import ij.io.Opener;
-import ij.io.OpenDialog;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.ArrayList;
 
 
 
 @Plugin(type = Command.class, menuPath = "Plugins>Fast Temporal Median 2")
 public class FTM2<T> implements PlugInFilter {
     
-        @Parameter
-	private ImagePlus image;
-                
+
         @Parameter
         private ImagePlus stack;
         
-        @Parameter
-        private ImagePlus vstack;
         
         @Parameter
 	public static String sourceDirectory="C:\\Users\\Martijn\\Desktop\\Thesis2020\\ImageJ\\test_images\\test_folder"; //Change before release
@@ -47,8 +37,6 @@ public class FTM2<T> implements PlugInFilter {
         @Parameter
 	public static String outputDirectory="C:\\Users\\Martijn\\Desktop\\Thesis2020\\ImageJ\\test_images\\output";//Change before release
         
-        @Parameter
-	public static String refimg="C:\\Users\\Martijn\\Desktop\\Thesis2020\\ImageJ\\test_images\\refstacksmall.tif";//Change before release
         
         @Parameter
         private String source_dir;
@@ -57,16 +45,14 @@ public class FTM2<T> implements PlugInFilter {
         private String target_dir;
         
         @Parameter
-        private String reference_image;
-        
-        @Parameter
-        private Integer[] slice_intervals;
+        private ArrayList<Integer> slice_intervals = new ArrayList<Integer>();
         
         @Parameter
         private Integer total_size = 0;
         
-        @Parameter
-        private int stack_height, stack_width, bitDepth;
+        @Parameter 
+        ArrayList<ImagePlus> vstacks = new ArrayList<ImagePlus>();
+
         
 	@Override
 	public int setup(String arg, ImagePlus imp) {
@@ -84,7 +70,7 @@ public class FTM2<T> implements PlugInFilter {
 
 		gd.addDirectoryField("Source directory", sourceDirectory, 50);
 		gd.addDirectoryField("Output directory", outputDirectory, 50);
-                gd.addFileField("Reference Image", refimg, 50);
+
                 gd.showDialog();
 		
 		// Exit when canceled
@@ -93,11 +79,11 @@ public class FTM2<T> implements PlugInFilter {
                 
                 sourceDirectory = gd.getNextString();
 		outputDirectory = gd.getNextString();
-                refimg = gd.getNextString();
+
 
                 source_dir = sourceDirectory;
                 target_dir = outputDirectory;
-                reference_image = refimg;
+
                 
 		if (null == source_dir) {
                     IJ.error("Error: No source directory was provided.");
@@ -107,10 +93,7 @@ public class FTM2<T> implements PlugInFilter {
                     IJ.error("Error: No output directory was provided.");
                     return DONE;
 		}
-                if (null == reference_image) {
-                    IJ.error("Error: No reference image was provided. Take a single frame from the stack and put it in a seperate location");
-                    return DONE;
-		}
+
 		
                 source_dir = source_dir.replace('\\', '/');
 		if (!source_dir.endsWith("/")) source_dir += "/";
@@ -118,53 +101,41 @@ public class FTM2<T> implements PlugInFilter {
 		target_dir = target_dir.replace('\\', '/');
 		if (!target_dir.endsWith("/")) target_dir += "/";
                 
-                reference_image = reference_image.replace('\\', '/');
+
 
                 
 		if( (new File( source_dir )).exists() == false ){
                     IJ.error("Error: source directory " + source_dir + " does not exist.");
                     return DONE;
 		}
-                if( (new File( reference_image )).exists() == false ){
-                    IJ.error("Error: reference image " + reference_image + " does not exist.");
-                    return DONE;
-		}
+
 		
                 if ((new File( target_dir )).exists() == false){
                     if(!new File(target_dir).mkdir()) {
                         IJ.error("Error: Failed to create target directory " + target_dir);
                     }
                 }
+                             
+                File[] listOfFiles = new File(source_dir).listFiles();               
                 
-		ImagePlus ref = IJ.openImage(reference_image);
-                //ImagePlus img = FolderOpener.open(source_dir, "virtual"); //doesnt open as virtual stack, funny huh
-                stack_height = ref.getHeight();
-                stack_width = ref.getWidth();
-                bitDepth = ref.getBitDepth();
-                
-                ColorModel cm = ref.getProcessor().getColorModel();
-                
-                File folder = new File(source_dir);
-                File[] listOfFiles = folder.listFiles();
-                ImagePlus[] vstacks = new ImagePlus[listOfFiles.length];
-                
-                
+                IJ.showStatus("Creating Virtualstack(s)");
                 for (int i = 0; i < listOfFiles.length; i++) {                       
                     if (listOfFiles[i].isFile()) {
-                        vstacks[i] = IJ.openVirtual(listOfFiles[i].getPath());
-                        
-                        System.out.print(Integer.toString(i) + ", " + listOfFiles[i].getPath() + ", " + Integer.toString(vstacks[i].getStackSize()) + "\n");
+                        vstacks.add(IJ.openVirtual(listOfFiles[i].getPath()));
+                        Integer current_stack_size = vstacks.get(i).getStackSize();
+                        total_size += current_stack_size;
+                        slice_intervals.add(current_stack_size);
+                        System.out.print(Integer.toString(i) + ", " + listOfFiles[i].getPath() + ", " + Integer.toString(current_stack_size) + "\n");
                     } else {
                         IJ.error("Error: File is not file.");
                     }
                 }
                 
-                stack = vstacks[0];
-
-                if(stack.getStackSize() == 1){
+                if(total_size <= 1){
                     IJ.error("Error: Stack must have size larger than 1.");
                 }
-                                                            
+                
+                System.gc();
 		return DOES_8G + DOES_16 + DOES_32 + NO_IMAGE_REQUIRED;
 	}
 
@@ -173,12 +144,15 @@ public class FTM2<T> implements PlugInFilter {
                 //This will first be getting a initial implementation to ensure it works
                 //If not yet implemented then, it will be ensured files>20GB will work without being loaded into memory
                 
-                ImagePlus img = new Duplicator().run(stack, 1, 1);         
-                img.show();
+                stack = vstacks.get(0);
                 
+                 
+                ImagePlus img = new ImagePlus("", stack.getStack().getProcessor(1));
+                img.show();
+                    
+
                 CLIJ2 clij2 = CLIJ2.getInstance();
 
-                // conversion
                 // conversion
                 ClearCLBuffer input = clij2.push(img);
                 ClearCLBuffer output = clij2.create(input);
@@ -203,11 +177,7 @@ public class FTM2<T> implements PlugInFilter {
 
                 final ImageJ IJ_Instance = new ImageJ();
                 
-                String dir = null;
-                //dir = "C:\\Users\\Martijn\\Desktop\\Thesis2020\\ImageJ\\test_images";
-		//ImagePlus image = IJ.openImage("C:\\Users\\Martijn\\Desktop\\Thesis2020\\ImageJ\\test_images\\stack_small.tif");
-		//image.show();
 
-		IJ.runPlugIn(FTM2.class.getName(), dir);
+		IJ.runPlugIn(FTM2.class.getName(), "");
 	}
 }
