ec87aae67ff563a28a164a77d39419931769244f
dr_njitram <martijn.gobes@gmail.com>
2020-09-11 15:03:48 +0200

    Add more gui
    add more robust loading
    add initial loading

--- src/main/java/com/wurgobes/imagej/FTM2.java
+++ src/main/java/com/wurgobes/imagej/FTM2.java
@@ -1,12 +1,13 @@
 package com.wurgobes.imagej;
 
+import fiji.util.gui.GenericDialogPlus;
 
 import ij.*;
-import fiji.util.gui.GenericDialogPlus;
-import ij.plugin.filter.ExtendedPlugInFilter;
 import ij.plugin.*;
+import ij.plugin.filter.ExtendedPlugInFilter;
 import ij.plugin.filter.PlugInFilterRunner;
-import ij.process.ImageProcessor;
+import ij.process.*;
+
 
 import org.scijava.command.Command;
 
@@ -17,10 +18,12 @@ import net.haesleinhuepf.clij2.CLIJ2;
 
 import org.scijava.plugin.*;
 import org.scijava.Priority;
-import org.scijava.table.*;
+
 
 import java.io.File;
 import java.util.*; 
+import jdk.nashorn.internal.ir.debug.ObjectSizeCalculator;
+
 
 
 @Plugin(type = Command.class, menuPath = "Plugins>Fast Temporal Median 2", label="FTM2", priority = Priority.VERY_HIGH)
@@ -28,7 +31,7 @@ public class FTM2 implements ExtendedPlugInFilter, Command {
     
 
     //@Parameter
-    private ImagePlus stack;
+    private ImageStack stack;
 
 
     //@Parameter
@@ -37,6 +40,9 @@ public class FTM2 implements ExtendedPlugInFilter, Command {
     //@Parameter
     public static String outputDirectory="C:\\Users\\Martijn\\Desktop\\Thesis2020\\ImageJ\\test_images\\output";//Change before release
 
+    public static Integer window = 50;
+    public static Integer start = 1;
+    public static Integer end = 0;
 
     //@Parameter
     private String source_dir;
@@ -51,8 +57,24 @@ public class FTM2 implements ExtendedPlugInFilter, Command {
     private Integer total_size = 0;
 
     //@Parameter 
-    private ArrayList<ImagePlus> vstacks = new ArrayList<ImagePlus>();
+    private ArrayList<ImageStack> vstacks = new ArrayList<ImageStack>();
+    
+    private Integer dimension;
+    
+    private Integer slice_size;
+    
+    private final long max_bytes = Runtime.getRuntime().maxMemory();
 
+    public Integer bit_depth;
+    
+    public long getFreeMemory(boolean am_i_the_garbage_man){
+        if (am_i_the_garbage_man){
+            System.gc();
+        }
+        return Runtime.getRuntime().freeMemory();
+    }
+    
+    
     @Override
     public int setup(String arg, ImagePlus imp) {
         //TODO
@@ -66,6 +88,9 @@ public class FTM2 implements ExtendedPlugInFilter, Command {
 
         gd.addDirectoryField("Source directory", sourceDirectory, 50);
         gd.addDirectoryField("Output directory", outputDirectory, 50);
+        gd.addNumericField("Window size", window, 0);
+        gd.addNumericField("Begin", start, 0);
+        gd.addNumericField("End (0 for all)", end, 0);
 
         gd.showDialog();
 
@@ -75,7 +100,9 @@ public class FTM2 implements ExtendedPlugInFilter, Command {
 
         sourceDirectory = gd.getNextString();
         outputDirectory = gd.getNextString();
-        
+        window = (int)gd.getNextNumber();
+        start = (int)gd.getNextNumber();
+        end = (int)gd.getNextNumber();
 
 
         source_dir = sourceDirectory;
@@ -114,14 +141,17 @@ public class FTM2 implements ExtendedPlugInFilter, Command {
         }
 
         File[] listOfFiles = new File(source_dir).listFiles();               
+        
 
         IJ.showStatus("Creating Virtualstack(s)");
         for (int i = 0; i < listOfFiles.length; i++) {                       
             if (listOfFiles[i].isFile()) {
-                vstacks.add(IJ.openVirtual(listOfFiles[i].getPath()));
-                Integer current_stack_size = vstacks.get(i).getStackSize();
+
+                vstacks.add(IJ.openVirtual(listOfFiles[i].getPath()).getStack());
+                Integer current_stack_size = vstacks.get(i).size();
+                slice_intervals.add(current_stack_size + total_size);
                 total_size += current_stack_size;
-                slice_intervals.add(current_stack_size);
+                
                 System.out.print(Integer.toString(i) + ", " + listOfFiles[i].getPath() + ", " + Integer.toString(current_stack_size) + "\n");
             } else {
                 IJ.error("Error: File is not file.");
@@ -131,13 +161,21 @@ public class FTM2 implements ExtendedPlugInFilter, Command {
         if(total_size <= 1){
             IJ.error("Error: Stack must have size larger than 1.");
         }
-
+        
+        dimension = vstacks.get(0).getWidth() * vstacks.get(0).getHeight(); //Amount of pixels per image
+        slice_size = dimension * (vstacks.get(0).getBitDepth()/8); //Byter per slice
+        bit_depth = vstacks.get(0).getBitDepth();
+        
+        if(end == 0) end = total_size;
+        if(window > total_size) window = total_size;
+        
         System.gc();
-        return DOES_8G + DOES_16 + DOES_32 + NO_IMAGE_REQUIRED;
+        
+        return DOES_8G + DOES_16 + DOES_32 + NO_IMAGE_REQUIRED + NO_UNDO;
     }
 
     @Override
-    public void run(){
+    public void run(){       
         ImagePlus Dummy = new ImagePlus();
         setup("", Dummy);
         run(Dummy.getProcessor());
@@ -147,32 +185,65 @@ public class FTM2 implements ExtendedPlugInFilter, Command {
     public void run(ImageProcessor ip) {
         //This will first be getting a initial implementation to ensure it works
         //If not yet implemented then, it will be ensured files>20GB will work without being loaded into memory
+        
+        
+        /*
+        tenet is to acces each slice as few times as possible
+        
+        get available memory
+        load half as many slices as fit (x by y times n) (the other half will be taken up by the reordered arrays)
+        for the entire loaded stack:
+            create vertically sliced arrays (v_pixels) (dimension arrays with n entries)
+        for every n entries:
+            take a i_window of desired size and take the median of v_pixels with that range and subtract that from the entry n (min 0) 
+                this median does not change for the first i_window slices and last i_window slices
+            look ahead and back as equally as possible (so from n-window/2 to n+window/2, with only forward and backward for the start and end respectively)   
+            when the buffered pixels run out, delete all unneeded data and load as much new data as can fit (to minimise memory writes)
+                one can overwrite the no longer needed pixels
+            
+            per n you reconstruct one slice by getting a single pixel per v_pixels array and putting those back
+            
+        */
+        //slice_size + 16 is the size of one frame when gotten with getPixels(n)
+        
+        long startTime = System.nanoTime();
+        
+        VirtualStack temp_stack = new VirtualStack(vstacks.get(0).getWidth(), vstacks.get(0).getHeight(), total_size);
+        short[] pixels = new short[dimension];
+        short[] new_pixels = new short[dimension];
+        Integer stack_index = 0;
+        Integer prev_stack_size = 0;
+        
+        Integer slices_that_fit = (int)(getFreeMemory(true)/slice_size)/2;
+        if (slices_that_fit > total_size) slices_that_fit = total_size;
+        
+        short[][] v_pixels = new short[dimension][slices_that_fit]; 
+        
+        Integer[] loaded_range = {1, slices_that_fit};
+        
+        
 
+        
+        System.out.println(total_size);
+        System.out.println(slices_that_fit);
+        System.out.println(ObjectSizeCalculator.getObjectSize(v_pixels));
+
+        //Populate array
         stack = vstacks.get(0);
-
-
-        ImagePlus img = new ImagePlus("", stack.getStack().getProcessor(1));
-        img.show();
-
-
-        CLIJ2 clij2 = CLIJ2.getInstance();
-
-        // conversion
-        ClearCLBuffer input = clij2.push(img);
-        ClearCLBuffer output = clij2.create(input);
-
-        // blur
-        float sigma = 20;
-        clij2.gaussianBlur(input, output, sigma, sigma);
-
-        ImagePlus result = clij2.pull(output);
-        result.show();
-
-        // free memory afterwards
-        input.close();
-        output.close();
-
-
+        for(int i = start; i < slices_that_fit; i++){
+            if(i > slice_intervals.get(stack_index)){
+                prev_stack_size += stack.size();
+                stack_index += 1;
+                stack = vstacks.get(stack_index);
+            }
+            pixels = (short[])stack.getPixels(i - prev_stack_size);
+            for (int j=0; j<dimension; j++){
+                v_pixels[j][i] = pixels[j];
+            }
+        }
+        System.out.println(ObjectSizeCalculator.getObjectSize(v_pixels));
+        long stopTime = (System.nanoTime()- startTime);
+        IJ.showMessage("Script took " + String.format("%.3f", (double)stopTime/1000000000) + " s");
     }
 
 
