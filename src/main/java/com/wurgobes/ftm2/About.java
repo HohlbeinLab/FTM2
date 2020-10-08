e51827f29c5a886ab736812e4b93a5a0b2d97bdf
dr_njitram <martijn.gobes@gmail.com>
2020-10-08 10:50:57 +0200

    Include version number in the About page

--- src/main/java/com/wurgobes/ftm2/About.java
+++ src/main/java/com/wurgobes/ftm2/About.java
@@ -7,7 +7,8 @@ public class About implements PlugIn {
 
     @Override
     public void run(String arg) {
-        String content = "Developed by Martijn Gobes at the Holhbein Lab.\nMore information can be found at https://github.com/HohlbeinLab/FTM2";
+        String content = "Developed by Martijn Gobes at the Holhbein Lab.\nMore information can be found at https://github.com/HohlbeinLab/FTM2\n" +
+                "Current Version: 0.9.2";
 
         GenericDialog gd = new GenericDialog("About");
         gd.addMessage(content);
